09aaf1dc181c2bc13c915314687e9a8067859ad1
Malik Samansya <maliksamansya206@gmail.com>
2023-09-01 09:45:07 +0700

    chore: skeleton

---
+++ server-side/seeders/20230615081938-seed-users.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { encryptPassword } = require('../helpers/bcrypt');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const data = require("../data/users.json");
+    const users = data.map((user) => {
+      user.createdAt = user.updatedAt = new Date();
+      user.password = encryptPassword(user.password);
+      return user;
+    });
+
+    await queryInterface.bulkInsert("Users", users, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
---
+++ server-side/seeders/20230615082011-seed-category.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const data = require("../data/categories.json");
+    const categories = data.map((category) => {
+      category.createdAt = category.updatedAt = new Date();
+      return category;
+    });
+
+    await queryInterface.bulkInsert("Categories", categories, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Categories", null, {});
+  },
+};
